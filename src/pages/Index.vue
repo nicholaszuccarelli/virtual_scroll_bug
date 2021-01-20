00d8c0ec6dd22a6c7f171e86f5d660a08fd11b64
Nick Zuccarelli <nicholaszuccarelli@gmail.com>
2021-01-20 14:49:35 +1100

    Quasar bug

---
+++ src/pages/Index.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page>
+    <q-item v-ripple tag="label">
+      <q-item-section avatar>
+        <q-radio v-model="messageType" val="CUSTOM" color="teal" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Custom Message</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-item v-ripple tag="label">
+      <q-item-section avatar>
+        <q-checkbox v-model="messageType" val="CUSTOM" color="teal" />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>Custom Message</q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-virtual-scroll
+      :items="rowCountArray"
+      style="height: 700px;"
+    >
+      <template v-slot="{ item }">
+        <q-item>
+          <q-item-section>
+            <q-input
+              :value="item"
+            />
+          </q-item-section>
+        </q-item>
+      </template>
+    </q-virtual-scroll>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      virtualScrollElements: 1000,
+      messageType: 'OTHER'
+    }
+  },
+  computed: {
+    rowCountArray () {
+      return [...Array(this.virtualScrollElements).keys()]
+    }
+  }
+}
+</script>
